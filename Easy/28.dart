8c970459f73da8d1a3281206ce5d594c3b562a1b
syamsundar662 <syamsundar662@gmail.com>
2024-09-30 23:22:03 +0530

    28

---
+++ Easy/28.dart
@@ -0,0 +1,8 @@
+class Solution {  
+  int strStr(String haystack, String needle) { 
+    for(int i = 0; i < haystack.length;i++ ) {
+     return haystack.indexOf(needle);
+    }
+    return -1;
+  }
+}
