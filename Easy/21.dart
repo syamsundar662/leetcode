453bebaf1e2b49f0bad9647a1a8eb2b12a5d5e38
syamsundar662 <syamsundar662@gmail.com>
2024-10-07 00:15:55 +0530

    .

--- Easy/21.dart
+++ Easy/21.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 /**
  * Definition for singly-linked list.
  * class ListNode {
@@ -8,17 +10,25 @@
  */
 class Solution {
   ListNode? mergeTwoLists(ListNode? list1, ListNode? list2) {
+    if (list1 == null) return list2;
+    if (list2 == null) return list1;
 
-      if(list1== null) return list2 ;
-      if(list2 == null) return list1 ;
+    if (list1.val! < list2.val!) {
+      list1.next = mergeTwoLists(list1.next, list2);
+      return list1;
+    } else {
+      list2.next = mergeTwoLists(list1, list2.next);
+      return list2;
+    }
+  }
+}
 
-     if (list1.val < list2.val){
-         list1.next = mergeTwoLists(list1.next , list2) ;
-         return list1 ;
-     }
-     else{
-         list2.next = mergeTwoLists(list1 , list2.next) ;
-         return list2 ;
-     }
+//its not default class by Leetcode
+class ListNode {
+  int? val;
+  ListNode? next;
+  ListNode(int value) {
+    this.val = value;
+    next = null;
   }
 }
